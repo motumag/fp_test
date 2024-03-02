e81576cf6132cf5e77f0d0f77b252de89006871d
motumag <motumagishu27@gmail.com>
2024-03-02 21:43:49 +0300

    user reducer is added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: { user: userReducer },
   //   reduces some error in redux
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
